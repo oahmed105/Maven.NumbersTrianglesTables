84cd198c94f09e9f2421e35405725b6dc8fa55df
oahmed105 <oahmed10598@gmail.com>
2023-07-08 20:47:09 -0400

    All test passed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,17 +2,30 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-
-        return null;
+                return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
 
-        return getMultiplicationTable(5);
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
+        StringBuilder sb = new StringBuilder();
+        String table = "";
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                if (i * j < 100) {
+                    sb.append(table).append(" ");
+                    if (i * j < 10) {
+                        sb.append(table).append(" ");
+                    }
+                }
+                sb.append(table).append(String.valueOf(i * j)).append(" |");
+            }
+            sb.append(table).append("\n");
+        } return sb.toString();
 
-        return getMultiplicationTable(10);
+
     }
 }
